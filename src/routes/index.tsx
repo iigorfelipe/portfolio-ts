5b5ca157f35e81c123994a7ab02ea2f42f84256b
Igor <iigorfelipe@gmail.com>
2022-09-21 15:24:58 -0300

    ✨ feat: redirect default route '/' to '/portfolio-ts'

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Main from '../pages/Main';
@@ -8,7 +8,8 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Main /> } />
+        <Route path='/' element={ <Navigate to='/portfolio-ts' /> } />
+        <Route path='/portfolio-ts' element={ <Main /> } />
         <Route path='/about' element={ <About /> } />
         <Route path='/contact' element={ <Contact /> } />
         <Route path='/portfolio' element={ <Portfolio /> } />
