273b1e1d093481353d8fdd69b4e5b22c2ec82632
Igor <iigorfelipe@gmail.com>
2022-09-21 13:18:32 -0300

    ✨ feat: create array with project information

---
+++ src/helpers/projects.ts
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    id: 1,
+    title: "e-Sports",
+    githubUrl: "https://github.com/iigorfelipe/esports",
+    img: '/desktop1.png',
+  },
+  {
+    id: 2,
+    title: "e-Sports",
+    githubUrl: "https://github.com/iigorfelipe/esports",
+    img: '/desktop2.png',
+  },
+  {
+    id: 3,
+    title: "Calculate",
+    githubUrl: "https://github.com/iigorfelipe/softeo-challenge",
+    img: '/calculate1.png',
+  },
+  {
+    id: 4,
+    title: "Calculate",
+    githubUrl: "https://github.com/iigorfelipe/softeo-challenge",
+    img: '/calculate2.png',
+  },
+  {
+    id: 5,
+    title: "Buscar repositórios no Github",
+    githubUrl: "https://github.com/iigorfelipe/igor-desafio-react",
+    img: '/github.png',
+  },
+  {
+    id: 6,
+    title: "Lista de Tarefas",
+    githubUrl: "https://github.com/iigorfelipe/lets-organize",
+    img: '/list.png',
+  },
+  {
+    id: 7,
+    title: "Layout do YouTube",
+    githubUrl: "https://github.com/iigorfelipe/layout-yt",
+    img: '/yt.png',
+  },
+  {
+    id: 8,
+    title: "Weather App",
+    githubUrl: "https://github.com/iigorfelipe/weather-app",
+    img: '/weather.png',
+  },
+  {
+    id: 9,
+    title: "Portfólio",
+    githubUrl: "https://github.com/iigorfelipe/portfolio-ts",
+    img: '/portfolio.png',
+  },
+];
