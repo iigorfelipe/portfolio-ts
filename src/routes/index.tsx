39966662f5e106e166dbcbe7ec89f0bcc4a67fc7
Igor <iigorfelipe@gmail.com>
2022-09-20 22:09:52 -0300

    ✨ feat: implements routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Main from '../pages/Main';
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      {/* <Route path='/' element={ Main } />
-      <Route path='/portfolio-react' element={ Main } />
-      <Route path='/about' element={ About } />
-      <Route path='/contact' element={ Contact } />
-      <Route path='/portfolio' element={ Portfolio } />
-      <Route path='*' element={ NotFound } /> */}
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Main /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/contact' element={ <Contact /> } />
+        {/* <Route path='/portfolio' element={ Portfolio } /> */}
+        {/* <Route path='*' element={ NotFound } /> */}
+      </Routes>
+    </BrowserRouter>
   )
 };
 
