c45bc94c4ff45ec85f8ee1eac2dd1bcc2b689bcc
Igor <iigorfelipe@gmail.com>
2022-09-20 21:08:25 -0300

    ✨ feat: create array with icons information

---
+++ src/helpers/icons.ts
@@ -0,0 +1,119 @@
+import {
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiReact,
+  SiRedux,
+  SiTypescript,
+  SiNodedotjs,
+  SiGit,
+  SiJest,
+  SiMocha,
+  SiGithub,
+  SiDocker,
+  SiTailwindcss,
+  SiExpress,
+  SiSequelize,
+  SiSass,
+  SiPrisma,
+  SiSqlite
+} from 'react-icons/si';
+import { GrMysql } from 'react-icons/gr';
+
+export const icons = [
+  {
+    id: 1,
+    name: 'JavaScript',
+    icon: SiJavascript,
+  },
+  {
+    id: 2,
+    name: 'HTML',
+    icon: SiHtml5,
+  },
+  {
+    id: 3,
+    name: 'CSS',
+    icon: SiCss3,
+  },
+  {
+    id: 4,
+    name: 'React',
+    icon: SiReact,
+  },
+  {
+    id: 5,
+    name: 'Redux',
+    icon: SiRedux,
+  },
+  {
+    id: 6,
+    name: 'TypeScript',
+    icon: SiTypescript,
+  },
+  {
+    id: 7,
+    name: 'NodeJS',
+    icon: SiNodedotjs,
+  },
+  {
+    id: 8,
+    name: 'MySQL',
+    icon: GrMysql,
+  },
+  {
+    id: 9,
+    name: 'Git',
+    icon: SiGit,
+  },
+  {
+    id: 10,
+    name: 'Jest',
+    icon: SiJest,
+  },
+  {
+    id: 11,
+    name: 'Mocha',
+    icon: SiMocha,
+  },
+  {
+    id: 12,
+    name: 'GitHub',
+    icon: SiGithub,
+  },
+  {
+    id: 13,
+    name: 'Docker',
+    icon: SiDocker,
+  },
+  {
+    id: 14,
+    name: 'Express',
+    icon: SiExpress,
+  },
+  {
+    id: 15,
+    name: 'Sequelize',
+    icon: SiSequelize,
+  },
+  {
+    id: 16,
+    name: 'Tailwind',
+    icon: SiTailwindcss,
+  },
+  {
+    id: 17,
+    name: 'Sass',
+    icon: SiSass,
+  },
+  {
+    id: 18,
+    name: 'Prisma',
+    icon: SiPrisma,
+  },
+  {
+    id: 19,
+    name: 'SQLite',
+    icon: SiSqlite,
+  },
+];
