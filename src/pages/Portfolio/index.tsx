f11de3c6ce2593cb46cdd9e8e1f74a89e2ee4033
Igor <iigorfelipe@gmail.com>
2022-09-21 13:21:29 -0300

    ✨ feat: implements Portfolio page

---
+++ src/pages/Portfolio/index.tsx
@@ -0,0 +1,13 @@
+import Carousel from '../../components/Carousel';
+import NavigationBar from '../../components/NavigationBar';
+
+const Portfolio = () => {
+  return (
+    <>
+      <NavigationBar />
+      <Carousel />
+    </>
+  )
+};
+
+export default Portfolio;
