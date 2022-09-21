44763230db22137ff091efdea973ffe43233541e
Igor <iigorfelipe@gmail.com>
2022-09-20 22:15:17 -0300

    ⚡️ feat: add target as '_blank' in link to Trybe website

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -11,7 +11,7 @@ const About = () => {
 
         <C.AboutText>
           <C.Paragraph>Olá, meu nome é Igor, sou apaixonado por <C.Span>tecnologia</C.Span> e muitas nerdices, eu amo estudar sobre o <C.Span>desenvolvimento web</C.Span> e fico fascinado o quão rapido essa área evolui, isso me incentiva a estudar mais ainda.</C.Paragraph>
-          <C.Paragraph>Já estou no final do meu curso na <C.Link href='https://www.betrybe.com/'>Trybe</C.Link> que é uma escola de <C.Span>programação</C.Span> que tem o foco no desenvolvimento web <C.Span>full stack</C.Span>, que é exatamente o que desejo. Eu amo estudar tanto sobre <C.Span>frontend</C.Span> quanto o <C.Span>backend</C.Span>.</C.Paragraph>
+          <C.Paragraph>Já estou no final do meu curso na <C.Link href='https://www.betrybe.com/' target='_blank' rel='noreferrer'>Trybe</C.Link> que é uma escola de <C.Span>programação</C.Span> que tem o foco no desenvolvimento web <C.Span>full stack</C.Span>, que é exatamente o que desejo. Eu amo estudar tanto sobre <C.Span>frontend</C.Span> quanto o <C.Span>backend</C.Span>.</C.Paragraph>
           <C.Paragraph>A direita está algumas tecnologias que estudei fora e dentro da Trybe, e você pode conferir meus certificados em meu <C.Link href='https://www.linkedin.com/in/iigor-felipe/details/certifications/' target='_blank' rel='noreferrer'>Linkedin</C.Link>.</C.Paragraph>
         </C.AboutText>
 
